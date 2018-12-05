ce566e8b3e0b76b74249f8c0636dd309255945da
smgd <soapmangoesdown@gmail.com>
2018-12-05 16:16:19 +0300

    migrate to psql

--- bureau_django/settings.py
+++ bureau_django/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'bureau_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carsdjango',
+        'USER': 'soapman',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
